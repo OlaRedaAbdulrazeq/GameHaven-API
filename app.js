1cb8511dad0bef27548805a9e367385318311235
OlaRedaAbdulrazeq <olaabdelrazeq@std.mans.edu.eg>
2025-06-17 03:21:54 +0300

    added morgan middleware

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ const app = express();
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
-
+app.use(morgan('dev'));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
